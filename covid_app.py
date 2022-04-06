c5f346f3e80b501236143a6dfc3ec5dd0acd83af
1colete <nwcolete@gmail.com>
2022-04-06 20:27:09 -0300

    first commit

---
+++ covid_app.py
@@ -0,0 +1,90 @@
+# from soupsieve import select
+import streamlit as st
+import joblib
+import pickle
+import pandas as pd
+import numpy as np
+import os
+# import sklearn
+
+#import sqlite3
+#from monitor_for_app import *
+# from variables import *
+#from datetime import datetime
+st.set_page_config(page_title = 'Previsor de COVID-19')
+
+@st.cache
+def load_data():
+    current_path = os.getcwd()
+    dados_path = os.path.join(current_path, 'dados\cleaned\df_preped.pkl')
+    df = pd.read_pickle(dados_path)
+    return df
+
+def load_model():
+	# load_model = joblib.load(open(os.path.join(model),"rb"))
+    current_path = os.getcwd()
+    model_path = os.path.join(current_path, 'models/clf_tuned.pkl')
+    load_model = joblib.load(open(model_path, 'rb'))
+    return load_model
+
+# load data
+# df = load_data()
+
+def main():
+    options = ['Início', 'Dados' ,'Predição','Sobre']
+    page_option = st.sidebar.selectbox('Páginas', options)
+    
+    if page_option == 'Início':
+        st.title('Previsor COVID-19 ')
+        # st.markdown(home_page_text, unsafe_allow_html = True)
+        
+    elif page_option == 'Dados':
+        st.title('Mais informações sobre o conjunto de dados')
+        # st.markdown(data_description, unsafe_allow_html = True)
+
+        # with st.expander('Estatisticas', expanded = False):
+        #     st.write('Podemos ver estatisticas para as colunas numéricas.')
+        #     st.write(df.describe())
+
+        # with st.expander('Valores Targets'):
+        #     st.write(f'Temos {round(df.obito.value_counts(normalize = True)*100, 2)[1]} % de obtidos na base.')
+        #     st.write(round(df.obito.value_counts(normalize = True)*100, 2))
+
+    elif page_option == 'Predição':
+        st.title('Predição')
+        
+        cardiopatia = st.checkbox('cardiopatia')
+        diabetes = st.checkbox('diabetes')
+        doenca_neurologica = st.checkbox('Doenças neurológicas')
+        obesidade = st.checkbox('obesidade')
+        outros_fatores_de_risco = st.checkbox('outros_fatores_de_risco')
+        idade = st.slider('Idade: ', 0, 110, 29)
+        cs_sexo = st.selectbox( 'Sexo', 
+                ('Masculino', 'Feminino'))
+        if cs_sexo == 'Masculino':
+            cs_sexo = True
+        else:
+            cs_sexo = False
+
+        selected_options = [cardiopatia, diabetes, doenca_neurologica, obesidade, outros_fatores_de_risco , idade , cs_sexo]
+
+        sample_input = np.array(selected_options).reshape(1,-1)
+        model = load_model()
+
+        if st.button('Submit'):
+            # class prediction
+            prediction_class = model.predict(sample_input)
+            if prediction_class == 1:
+                prediction_class = 'ALTO RISCO'
+            else:
+                prediction_class = 'BAIXO RISCO'
+
+            st.success(f"Paciente com {prediction_class} de obito")
+               
+    else:
+        st.title('Sobre')
+        # st.markdown(about_text, unsafe_allow_html = True)
+        
+    
+main()
+
---
+++ variables.py
@@ -0,0 +1,79 @@
+# SECTION HOMEPAGE
+
+# import pandas as pd
+# df = pd.read_pickle('dados/cleaned/df_preped.pkl')
+
+
+home_page_text = """
+<div style="text-align: justify"> 
+
+## Objetivo:
+Este aplicativo tem o objetivo de ajudar na triagem de pacientes com COVID-19, baseado no histórico médico é capaz de reconhecer casos possívelmente graves e ajudar no processo de decisão de internação.
+
+## Sintomas
+A Covid-19 é uma doença infecciosa causada pelo coronavírus (Sars-CoV-2) e tem como principais sintomas:
+- Febre
+- Cansaço
+- Tosse seca. 
+
+Alguns pacientes podem apresentar 
+- Dores
+- Congestão nasal
+- Dor de cabeça
+- Conjuntivite
+- Dor de garganta
+- Diarreia
+- Perda de paladar ou olfato
+- Erupção cutânea na pele ou descoloração dos dedos das mãos ou dos pés. 
+  
+Esses sintomas geralmente são leves e começam gradualmente. Algumas pessoas são infectadas, mas apresentam apenas sintomas muito leves ou nao apresentam sintomas.
+
+## Obs:
+A maioria das pessoas (cerca de 80%) se recupera da doença sem precisar de tratamento hospitalar. Uma em cada seis pessoas infectadas por Covid-19 fica gravemente doente e desenvolve dificuldade de respirar. As pessoas idosas e as que têm outras condições de saúde como pressão alta, problemas cardíacos e do pulmão, diabetes ou câncer, têm maior risco de ficarem gravemente doentes. No entanto, qualquer pessoa pode pegar a Covid-19 e ficar gravemente doente.
+
+</div>
+
+"""
+
+
+#  SECTION DATA
+
+data_description = """
+<div style = "text-align: justify">O conjunto de dados consiste em algumas condições de saúde preexistentes para determiar o risco de óbito por COVID-19. As variáveis analisadas incluem idade, sexo, cardiopatia, diabetes, doencas neurologicas, obesidade, e outros fatores de risco.
+
+Fonte dos dados de contaminação por COVID-19 https://www.seade.gov.br/coronavirus/
+</div>
+<br>
+"""
+
+# age_min_value = int(df['idade'].min())
+# age_max_value = int(df['idade'].max())
+
+# SECTION ABOUT
+about_text = """
+<div style="text-align: justify">
+
+## Projeto desenvolvido por:
+- Alexandre Antunes
+- Alexandre Colete Silva
+- Emerson Araujo
+- Eduardo Hagiwara
+- Vinicio Lima Pedrosa Lins
+
+## Proposta e Metodologia:
+A proposta desse aplicativo é prever casos graves de COVID-19 usando como base informações de idade, sexo e comorbidades pré-existentes. Foi utilizado um modelo de arvore de decisão com seus hiper parâmetros otimizados.
+
+
+## Fontes:
+- https://www.saude.df.gov.br/coronavirus/
+- https://cidades.ibge.gov.br/brasil/sp/panorama
+- https://github.com/ipeaGIT/geobr
+- https://ipeagit.github.io/geobr/articles/python-intro/py-intro-to-geobr.html
+- https://basedosdados.org/dataset/br-ibge-populacao
+- https://www.kaggle.com/rubensmau/covid-19-deaths-per-capita
+- https://towardsdatascience.com/analysis-of-covid-19-using-per-capita-data-f8f9a31a2c4d
+
+
+
+</div>
+"""
